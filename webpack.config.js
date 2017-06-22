94aa4388af2b94887dbedf53fd7aeb90896bef51
kunapot <kunapot@thinknet.co.th>
2017-06-22 13:47:51 +0700

    init project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require('path')
+
+module.exports = {
+  entry: resolve(__dirname, 'src/index.js'),
+  output: {
+    publicPath: 'dist/',
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /(node_modules)/,
+      },
+    ],
+  },
+  devServer: {
+    host: '0.0.0.0',
+    inline: true,
+    port: 8081,
+    historyApiFallback: true,
+  },
+}
